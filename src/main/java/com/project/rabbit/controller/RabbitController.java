906ba0deb3d5e01d6e7b9732cb89346c31108eed
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-05 07:14:44 -0300

    Publishing message to headers exchange using rabbit template

--- src/main/java/com/project/rabbit/controller/RabbitController.java
+++ src/main/java/com/project/rabbit/controller/RabbitController.java
@@ -1,9 +1,16 @@
 package com.project.rabbit.controller;
 
 import com.project.rabbit.model.People;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -12,10 +19,35 @@ public class RabbitController {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
+//    @GetMapping("/send/{name}")
+//    public String testAPI(@PathVariable("name") String name) {
+//        People p = new People(1L, name);
+//        rabbitTemplate.convertAndSend("Mobile", p);
+//        rabbitTemplate.convertAndSend("Direct-Exchange", "mobile", p);
+//        rabbitTemplate.convertAndSend("Fanout-Exchange", "", p);
+//        rabbitTemplate.convertAndSend("Topic-Exchange", "tv.mobile.ac", p);
+//        return "You message was send with success!";
+//    }
+
     @GetMapping("/send/{name}")
-    public String testAPI(@PathVariable("name") String name) {
+    public String testAPI(@PathVariable("name") String name) throws IOException {
         People p = new People(1L, name);
-        rabbitTemplate.convertAndSend("Mobile", p);
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out = new ObjectOutputStream(bos);
+        out.writeObject(p);
+        out.flush();
+        out.close();
+
+        byte[] byteMessage = bos.toByteArray();
+        bos.close();
+
+        Message msg = MessageBuilder.withBody(byteMessage)
+                .setHeader("item1", "mobile")
+                .setHeader("item2", "television").build();
+
+        rabbitTemplate.send("Headers-Exchange", "", msg);
+
         return "You message was send with success!";
     }
 
