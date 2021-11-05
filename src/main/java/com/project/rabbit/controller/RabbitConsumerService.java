142b9508e5bb9622012cb78c24149e0f2a236229
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-05 08:16:10 -0300

    Rabbit Listerner with Spring Boot & Headers Exchange

--- src/main/java/com/project/rabbit/controller/RabbitConsumerService.java
+++ src/main/java/com/project/rabbit/controller/RabbitConsumerService.java
@@ -4,11 +4,21 @@ import com.project.rabbit.model.People;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 @Service
 public class RabbitConsumerService {
 
     @RabbitListener(queues = "Mobile")
-    public void getMessage(People p) {
+    public void getMessage(byte[] msg) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(msg);
+        ObjectInput in = new ObjectInputStream(bis);
+        People p = (People) in.readObject();
+        in.close();
+        bis.close();
         System.out.println(p.getName());
     }
 
